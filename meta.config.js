985c32d5c7fa96eb2c5bb867c0090bab81408fc4
liquiid <liquiid727@outlook.com>
2024-04-24 03:45:13 +0800

    init

--- meta.config.js
+++ meta.config.js
@@ -1,4 +1,24 @@
+const character = {
+  name: "Liquiid梨亏得",
+  skills: ["react", "js", "next", "react", "astro","node.js"],
+  wechatId:'xiaolin07laixi',
+  phoneNumber:'15868755240',
+  email:'liquiid727@outlook.com',
+  city:"Hangzhou,China",
+  github:"https://github.com/Liquiid727",
+  yuque:"https://liquiid727.github.io/",
+}
+
+
+
+
 export default {
-  name: "阿崔1234",
-  skills: ["Vue3", "js", "ts", "react", "solidjs"],
+  name: character.name,
+  skills: character.skills,
+  wechatId:btoa(character.wechatId),
+  phoneNumber:btoa(character.phoneNumber),
+  email:character.email,
+  city:"Hangzhou,China",
+  github:"https://github.com/Liquiid727",
+  yuque:"https://liquiid727.github.io/",
 };
